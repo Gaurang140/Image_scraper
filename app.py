6e83616ebd88f73408fa75785697cb2b5dc86b87
Gaurang <gaurannggirimeghanathi@gmail.com>
2023-08-01 11:49:46 +0200

    final

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def index():
 
 
 
-                            # fake user agent to avoid getting blocked by Google
+                           
                     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"}
 
                             # fetch the search results page
